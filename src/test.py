90fd8e58bdaee6b38578ee42df6b3ba5ec3738ca
talafek96 <talafek@campus.technion.ac.il>
2021-12-18 22:50:31 +0200

    Add test

--- src/test.py
+++ src/test.py
@@ -1,4 +1,7 @@
 from prepare import prepare_data
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import inspect
 
 
 class bcolors:
@@ -14,7 +17,43 @@ class bcolors:
 
 
 def main():
-    pass
+    dataset = pd.read_csv('csv/virus_data.csv')
+    train, test = train_test_split(dataset, test_size=0.2, random_state=10)
+    train.reset_index(inplace=True)
+    test.reset_index(inplace=True)
+    print(f'Train shape is {train.shape} and test shape is {test.shape}')
+
+    train_clean = prepare_data(train, train)
+    test_clean = prepare_data(test, train)
+    print(
+        f'New train shape is {train_clean.shape} and new test shape is {test_clean.shape}')
+
+    print(f'Train head:\n{train_clean.head(10)}\n\n')
+    print(f'Test head:\n{test_clean.head(10)}\n\n')
+    check_na(train_clean)
+    check_na(test_clean)
+
+    # Export to csv files:
+    train_path = 'csv/virus_train.csv'
+    test_path = 'csv/virus_test.csv'
+    train_clean.to_csv(train_path, index=False)
+    test_clean.to_csv(test_path, index=False)
+
+
+def check_na(df: pd.DataFrame):
+    this_function_name = inspect.currentframe().f_code.co_name
+
+    failed = False
+    errors = str()
+    for column in df:
+        df_col_na = df[column].isna()
+        if any(df_col_na):
+            failed = True
+            errors += f'\tfound {sum(df_col_na)} NaN values in column {column}.'
+    if not failed:
+        print_success(this_function_name)
+    else:
+        print_fail(this_function_name, errors)
 
 
 def print_success(func_name: str):
