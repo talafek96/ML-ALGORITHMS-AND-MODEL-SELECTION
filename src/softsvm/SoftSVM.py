032ccfed5906aff9f6a37060d9a06bcabe2b103c
talafek96 <talafek@campus.technion.ac.il>
2021-12-25 21:29:37 +0200

    Upload SoftSVM module

---
+++ src/softsvm/SoftSVM.py
@@ -0,0 +1,161 @@
+from sklearn.base import BaseEstimator, ClassifierMixin
+import numpy as np
+
+
+class SoftSVM(BaseEstimator, ClassifierMixin):
+    """
+    Custom C-Support Vector Classification.
+    """
+
+    def __init__(self, C: float, lr: float = 1e-5):
+        """
+        Initialize an instance of this class.
+        ** Do not edit this method **
+
+        :param C: inverse strength of regularization. Must be strictly positive.
+        :param lr: the SGD learning rate (step size)
+        """
+        self.C = C
+        self.lr = lr
+        self.batch_size = 32
+        self.w = None
+        self.b = 0.0
+
+    # Initialize a random weight vector
+    def init_solution(self, n_features: int):
+        """
+        Randomize an initial solution (weight vector)
+        ** Do not edit this method **
+
+        :param n_features:
+        """
+        self.w = np.random.randn(n_features)
+        self.b = 0.0
+
+    @staticmethod
+    def loss(w, b: float, C: float, X, y):
+        """
+        Compute the SVM objective loss.
+
+        :param w: weight vector for linear classification; array of shape (n_features,)
+        :param b: bias scalar for linear classification
+        :param C: inverse strength of regularization. Must be strictly positive.
+        :param X: samples for loss computation; array of shape (n_samples, n_features)
+        :param y: targets for loss computation; array of shape (n_samples,)
+        :return: the Soft SVM objective loss (float scalar)
+        """
+        margins = (X.dot(w) + b).reshape(-1, 1)
+        hinge_inputs = np.multiply(margins, y.reshape(-1, 1))
+
+        norm = np.linalg.norm(w)
+
+        hinge_loss = sum(list(map(lambda x: x if x > 0 else 0,
+                         (1 - hinge_inputs).reshape(1, -1)[0])))
+        loss = norm*norm + C*hinge_loss
+        return loss
+
+    @staticmethod
+    def subgradient(w, b: float, C: float, X, y):
+        """
+        Compute the (analytical) SVM objective sub-gradient.
+
+        :param w: weight vector for linear classification; array of shape (n_features,)
+        :param b: bias scalar for linear classification
+        :param C: inverse strength of regularization. Must be strictly positive.
+        :param X: samples for loss computation; array of shape (n_samples, n_features)
+        :param y: targets for loss computation; array of shape (n_samples,)
+        :return: a tuple with (the gradient of the weights, the gradient of the bias)
+        """
+        # calculate the analytical sub-gradient of soft-SVM w.r.t w and b
+        g_w = None
+        g_b = 0.0
+
+        # partial_b_inputs: C*f(y * ((X dot w) + b)) * y = C*f(y*margins)*y = C*f(hinge_inputs)*y
+        def f_z(x): return list(map(lambda z: -1 if z < 1 else 0, x))
+
+        def cacl_partial_b_inputs(f=f_z):
+            '''
+            :param f: A function f that gets a vector as input and returns a result vector.
+            '''
+            margins = (X.dot(w) + b).reshape(-1, 1)
+            hinge_inputs = np.multiply(margins, y.reshape(-1, 1))
+            return C * np.multiply(f(hinge_inputs), y)
+
+        partial_b_inputs = cacl_partial_b_inputs()
+        g_b = sum(partial_b_inputs)
+
+        #    [a, b, c]     *   [[    x1     ],
+        #        ^              [    x2     ],  =  a*x1 + b*x2 + c*x3
+        #  partial_b_inputs     [    x3     ]]
+        X_part = partial_b_inputs.dot(X)
+        g_w = 2*w + X_part
+        return g_w, g_b
+
+    def fit_with_logs(self, X, y, max_iter: int = 2000, keep_losses: bool = True):
+        """
+        Fit the model according to the given training data.
+
+        :param X: training samples; array of shape (n_samples, n_features)
+        :param y: training targets; array of shape (n_samples,)
+        :param max_iter: number of SGD iterations
+        :param keep_losses:
+        :return: the training losses and accuracies during training
+        """
+        # Initialize learned parameters
+        self.init_solution(X.shape[1])
+
+        losses = []
+        accuracies = []
+
+        if keep_losses:
+            losses.append(self.loss(self.w, self.b, self.C, X, y))
+            accuracies.append(self.score(X, y))
+
+        # Iterate over batches
+        for iter in range(0, max_iter):
+            start_idx = (iter * self.batch_size) % X.shape[0]
+            end_idx = min(X.shape[0], start_idx + self.batch_size)
+            batch_X = X[start_idx: end_idx]
+            batch_y = y[start_idx: end_idx]
+
+            # Compute the (sub)gradient of the current *batch*
+            g_w, g_b = type(self).subgradient(
+                self.w, self.b, self.C, batch_X, batch_y)
+
+            # Perform a (sub)gradient step
+            # update the learned parameters correctly
+            self.w = self.w - self.lr*g_w
+            self.b = self.b - self.lr*g_b
+
+            if keep_losses:
+                losses.append(self.loss(self.w, self.b, self.C, X, y))
+                accuracies.append(self.score(X, y))
+
+        return losses, accuracies
+
+    def fit(self, X, y, max_iter: int = 2000):
+        """
+        Fit the model according to the given training data.
+        ** Do not edit this method **
+
+        :param X: training samples; array of shape (n_samples, n_features)
+        :param y: training targets; array of shape (n_samples,)
+        :param max_iter: number of SGD iterations
+        """
+        self.fit_with_logs(X, y, max_iter=max_iter, keep_losses=False)
+
+        return self
+
+    def predict(self, X):
+        """
+        Perform classification on samples in X.
+
+        :param X: samples for prediction; array of shape (n_samples, n_features)
+        :return: Predicted class labels for samples in X; array of shape (n_samples,)
+        """
+        # compute the predicted labels (+1 or -1)
+        def predict_row(xi):
+            return 1 if (self.w.dot(xi) + self.b) >= 0 else -1
+
+        y_pred = np.apply_along_axis(predict_row, axis=1, arr=X)
+        return y_pred
