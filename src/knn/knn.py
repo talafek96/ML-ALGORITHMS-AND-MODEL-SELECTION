92d203c4620db964edc2fa1a4baa748897678e37
talafek96 <talafek@campus.technion.ac.il>
2021-12-27 21:05:14 +0200

    Remove loops

--- src/knn/knn.py
+++ src/knn/knn.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from sklearn.base import BaseEstimator, ClassifierMixin
 from scipy.spatial import distance
-# from statistics import mode
+from scipy.stats import mode
 from collections import Counter
 import numpy as np
 
@@ -10,7 +10,7 @@ class kNN(BaseEstimator, ClassifierMixin):
     def __init__(self, n_neighbors: int = 5):
         self.n_neighbors = n_neighbors
 
-    def fit(self, X, y, k: Optional[int]=None):
+    def fit(self, X, y, k: Optional[int] = None):
         self.train_x = X
         self.train_y = y
         if k is not None:
@@ -21,12 +21,6 @@ class kNN(BaseEstimator, ClassifierMixin):
         # Note: You can use self.n_neighbors here
         dist_mat = distance.cdist(target_x, self.train_x, 'euclidean')
         ind = np.argpartition(dist_mat, self.n_neighbors, axis=1)
-        labeling_mat = np.array(
-            [self.train_y[ind[i][:self.n_neighbors]] for i in range(len(ind))])
-        predictions = np.array(
-            [type(self).__most_common(row) for row in labeling_mat])
+        predictions = mode(self.train_y[ind][:, :self.n_neighbors], axis=1)[
+            0].flatten()
         return predictions
-
-    @staticmethod
-    def __most_common(arr):
-        return Counter(arr).most_common(1)[0][0]
